16ead55583e4bca73fc28ea5251576c5e5bc0bfb
Haobo Chen <haobo.chen@appfolio.com>
2017-01-13 10:23:55 -0800

    Add CORS support

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,13 @@ module Dota2TickerApi
 
     # Config for rack_attack
     config.middleware.use Rack::Attack
+
+    # Config for rack_cors
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
